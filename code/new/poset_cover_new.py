6142de2e7e8e0ad3356494a17b901229fd4143b8
RexYuan <r1218r1218@gmail.com>
2025-01-03 01:43:16 +0800

    stuck

--- code/new/poset_cover_new.py
+++ code/new/poset_cover_new.py
@@ -1,3 +1,6 @@
+
+from itertools import permutations
+
 from pysat.formula import *
 from pysat.solvers import Solver
 
@@ -92,34 +95,87 @@ def nle_constraints(universe, name, lin):
         constraints.append( v.id((name,*r)) )
     return constraints
 
-# Setup solver
+def connected_poset_cover(lins):
+    omega = set(lins[0])
+    s = Solver(name='m22')
+
+    def get_swap(s):
+        if type(s) == str:
+            for i in range(len(s)-1):
+                yield s[:i]+s[i+1]+s[i]+s[i+2:]
+        else:
+            for i in range(len(s)-1):
+                yield s[:i]+(s[i+1],)+(s[i],)+s[i+2:]
+    bar = {''.join(p) for p in permutations(omega)} - set(lins)
+
+    # make k posets ; worst case is size of lins
+    for k in range(1, len(lins)+1):
+        print('k =', k)
+        s.new(name='m22')
+
+        # poset axioms : basic poset contraints
+        for i in range(k):
+            s.append_formula( poset_axioms(omega , str(i)) )
+
+        # extension constraints : forall l, exists p, p covers l
+        for l in lins:
+            tmp = []
+            for i in range(k):
+                tmp.append( le_constraints(omega, str(i), l) )
+            cnf_clauses, or_var = tseitin_or_and(tmp)
+            s.append_formula(cnf_clauses)
+            s.add_clause([or_var])
+
+        # non-extension constraints : forall not l, forall p, p does not cover l
+        for l in bar:
+            for i in range(k):
+                s.add_clause(nle_constraints(omega, str(i), l))
+
+        if s.solve():
+            for name in range(k):
+                name = str(name)
+                for x in omega:
+                    for y in omega-{x}:
+                        if v.id((name,x,y)) in s.get_model():
+                            print((name,x,y), v.id((name,x, y)))
+            break
+        else:
+            print('no solution for k =', k)
+
+# connected_poset_cover(['abc', 'acb', 'cba'])
+
 s = Solver(name='m22')
 
 # Create poset axioms for '1' and '2'
 a = poset_axioms(set('abc'), '1')
 b = poset_axioms(set('abc'), '2')
+# Append formulas
+s.append_formula(a)
+s.append_formula(b)
 
 # Constraints for linear orders
 l1 = le_constraints(set('abc'), '1', 'abc')
 l2 = le_constraints(set('abc'), '2', 'abc')
 l3 = le_constraints(set('abc'), '1', 'acb')
-l3 = le_constraints(set('abc'), '2', 'acb')
-l3 = le_constraints(set('abc'), '1', 'cba')
-l3 = le_constraints(set('abc'), '2', 'cba')
-
-# Append formulas
-s.append_formula(a)
-s.append_formula(b)
+l4 = le_constraints(set('abc'), '2', 'acb')
+l5 = le_constraints(set('abc'), '1', 'cba')
+l6 = le_constraints(set('abc'), '2', 'cba')
 
 # Add CNF constraints for combinations of linear orders
-f1 = [l1]
+f1 = [l1, l2]
 cnf_clauses, or_var = tseitin_or_and(f1)
 print(cnf_clauses, or_var)
 s.append_formula(cnf_clauses)
 s.add_clause([or_var])
 
-f2 = [l3]
+f2 = [l3, l4]
 cnf_clauses, or_var = tseitin_or_and(f2)
+print(cnf_clauses, or_var)
+s.append_formula(cnf_clauses)
+s.add_clause([or_var])
+
+f3 = [l5, l6]
+cnf_clauses, or_var = tseitin_or_and(f3)
 print(cnf_clauses, or_var)
 s.append_formula(cnf_clauses)
 s.add_clause([or_var])
